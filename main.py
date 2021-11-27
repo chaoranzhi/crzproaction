f2c9c5279d0e757a26d283c04a4da833d0a53b01
chaoranzhi <53324454+chaoranzhi@users.noreply.github.com>
2021-11-27 19:46:35 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+now_time = datetime.now()
+utc_time = datetime.utcnow()
+
+print(now_time)#当前时间，电脑显示的时间
+print(utc_time)#utc时间，伦敦时间
+
+with open('clash.yml','w+') as f:
+    f.writelines('{}'.format(now_time))
